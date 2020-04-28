4584b1788639270b96648709362aea3ef9d662c1
askrzatek <skrzatek.a@gmail.com>
2020-04-28 04:31:39 +0200

    tests suivants pour recuperer les ROI.mat de notre dossier toujours pas de reussite

--- MarsBaR_ROI_analysis.m
+++ MarsBaR_ROI_analysis.m
@@ -1,5 +1,4 @@
 % Extract Mean Percent Signal Change script.  Problem indicated with comment.
-*
 % MarsBar percent activation script
 % Before running this do the following
 % 1) make sure you have imported your ROIs from WFU and saved them
@@ -13,37 +12,58 @@ marsbar('on')
 % Set up the SPM defaults, just in case
 spm('defaults', 'fmri');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% BASIC STEPS IN ROI ANALYSIS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-spm_name = '/my/path/SPM.mat';
-roi_file = '/my/path/my_roi.mat';
-% Make marsbar design 
-objectD = mardo(spm_name);
-% Make marsbar ROI 
-objectR = maroi(roi_file);
-% Fetch data into marsbar data 
-objectY = get_marsy(R, D, 'mean');
-% Get contrasts from original 
-designxCon = get_contrasts(D);
-% Estimate design on ROI 
-dataE = estimate(D, Y);
-% Put contrasts from original design back into design 
-objectE = set_contrasts(E, xCon);
-% get design betasb = betas(E);
-% get stats and stuff for all contrasts into statistics 
-structuremarsS = compute_contrasts(E, 1:length(xCon));
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% BASIC STEPS IN ROI ANALYSIS
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% spm_name = '/my/path/SPM.mat';
+% roi_file = '/my/path/my_roi.mat';
+% % Make marsbar design 
+% objectD = mardo(spm_name);
+% % Make marsbar ROI 
+% objectR = maroi(roi_file);
+% % Fetch data into marsbar data 
+% objectY = get_marsy(R, D, 'mean');
+% % Get contrasts from original 
+% designxCon = get_contrasts(D);
+% % Estimate design on ROI 
+% dataE = estimate(D, Y);
+% % Put contrasts from original design back into design 
+% objectE = set_contrasts(E, xCon);
+% % get design betasb = betas(E);
+% % get stats and stuff for all contrasts into statistics 
+% structuremarsS = compute_contrasts(E, 1:length(xCon));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek','nifti');
 dirstat = fullfile(char(main_dir), 'secondlevel_ACTIVATION_PARK_S1');
 % dirgroup = fullfile(char(dirstat), {'PARKGAME_a', 'PARKGAME_c'});
 
-model_dir = fullfile(char(dirstat), 'ANOVA2x2_LxT');
-%rois_dir = fullfile(char(model_dir),'rois');
-rois_dir = r_mkdir(char(model_dir),'rois')
+%% creating ROIs from SPM thresholded images
+par.conname = 'Main_spe_LEFT_IMAGINARY_S1.*_roi.mat';
+par.subdir = 'ANOVA2x2_LxT';
+
+test_batch(par); % batch spm for loading a contrast and saving it in .mat in model dir %turns out not useful at all
+
+close all
+
+%%
+model_dir = fullfile(char(dirstat), par.subdir);
+rois_dir = get_subdir_regex(char(model_dir),'rois');
+
+%rois_dir = r_mkdir(char(model_dir),'rois')
 spm_name = fullfile(char(model_dir),'SPM.mat');
-roi_files = fullfile(model_dir,'roi.mat'); %paths to roi files - in cellstructure
+all_roi_files = dir(char(rois_dir));
+roi_file_names= {};
+for i = 1:length(all_roi_files)
+    if regexp(all_roi_files(i).name, par.conname)
+        roi_file_names = vertcat(roi_file_names, all_roi_files(i).name); %nope
+    else
+        fprintf('non admis\n')
+    end
+end
+
+roi_files = regexp(all_roi_file_names{:}, par.conname, 'match'); % names of rois files in a cell structure :: we just need their full paths
+roi_files = fullfile(rois_dir, roi_files);
 
 % set design from file
 %spm_names = spm_select(1, 'SPM.mat', 'Select SPM.mat'); useful only if SPM.mat undefined or not found
@@ -57,12 +77,15 @@ D = mardo(spm_name);
 
 %%
 
-R = cellstr(roi_files);
-R = maroi(R);
+V = spm_vol(roi_files);
+roi_name = char(addsuffixtofilenames(par.conname, '_ROI'));
+R = maroi_image(struct('vol',V, 'binarize', 0, 'func', 'img'));
+R = maroi_matrix(R);
+saveroi(R, roi_name);
 
 
 % Fetch data into marsbar data object
-mY  = get_marsy(R{1:length(roi_files)}, D, 'mean');
+mY  = get_marsy(R, D, 'mean');
 y = summary_data(mY);
 
 % Get contrasts from original design
