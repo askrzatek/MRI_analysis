55f5b0635f93693475a4e9bfd304c01cc470b6ef
askrzatek <skrzatek.a@gmail.com>
2020-07-30 17:53:00 +0200

    CLUSTER variable + new do_fsl_robust_mask_epi & bet + new tedana version better exec outside matvol + new coreg & normal routine + smooth + noise ROIs tissue maps normalized

--- preproc_tedana.m
+++ preproc_tedana.m
@@ -1,16 +1,20 @@
 % preproc_tedana
 
-clear all
 clc
+clear all
 
 %% Initialisation
+CLUSTER = 0;
 par.pct = 0;
-main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek','nifti_test');
+
+%main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek','nifti_test');
+main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test','PRISMA_REMINARY'); % pour comparer les résultats avec les VS
 
 e_PARKGAME = exam(main_dir,'PARKGAME');
-%e_REMINARY = exam(main_dir,'REMINARY_\w{2}_');
+e_REMINARY = exam(main_dir,'REMINARY_\w{2}_');
 
-e = e_PARKGAME; % (3:length(e_PARKGAME)); % choose specific
+%e = e_PARKGAME; % (3:length(e_PARKGAME)); % choose specific
+e = e_REMINARY;
 e.addSerie('ACTIVATION$','run_ACTIVATION',1)
 e.addSerie(        'RS$','run_RS'        ,1)
 
@@ -58,28 +62,42 @@ end
 
 clear par
 
+if CLUSTER
+    par.run      = 0;
+    par.sge      = 1;
+else
+    par.run      = 1;
+    par.sge      = 0;
+end
 par.fname        = 'meinfo'; % name of the .mat file that will be saved
-par.sge          = 0;
 par.redo         = 1;
-par.run          = 1;
+
 par.fake         = 0;
 
 run_list = e.getSerie('run');
 
 [meinfo, jobs] = job_sort_echos( run_list , par )
 
-meinfo.volume = e.getSerie('run').getVolume('^e')
-meinfo.anat   = e.getSerie('anat').getVolume('^s')
+e.gser('run').addVolume('^e','e',3);
+
+meinfo.volume = e.getSerie('run').getVolume('^e');
+meinfo.anat   = e.getSerie('anat').getVolume('^s');
 
 %% AFNI - vtd
 
 clear par
-par.blocks   = {'despike','tshift','volreg'}; % now codded : despike, tshift, align, volreg
-par.seperate = 1;                             % each volume is treated seperatly : useful when runs have different orientations
-par.execute  = 1;                             % execute afni_proc.py generated tcsh script file immidatly after the generation
 
-par.run      = 1;
-par.sge      = 0;
+par.blocks   = {'despike','tshift','volreg'}; % now codded : despike, tshift, align, volreg
+par.seperate    = 1;                             % each volume is treated seperatly : useful when runs have different orientations
+par.execute     = 1;                             % execute afni_proc.py generated tcsh script file immidatly after the generation
+par.write_nifti = 1;
+if CLUSTER
+    par.run     = 0;
+    par.sge     = 1;
+else
+    par.run     = 1;
+    par.sge     = 0;
+end
 
 par.jobname  = 'job_afni_proc_multi_echo';
 par.subdir   = 'afni';
@@ -103,45 +121,68 @@ e.getSerie('run').addVolume('^vtde3.nii','vtde3',1)
 [ec_vtd, ei_vtd] = e.removeIncomplete;
 e = ec_vtd;
 
-%% temporal mean
+%% do_fsl_robust_mask_epi
+
+fin = e.gser('run').gvol('^vtde1');
 
 clear par
-par.run   = 1;
-par.fake  = 0;
-par.sge   = 0;
-par.fsl_output_format = 'NIFTI';
-
-par.jobname = 'fslmean';
-vtde_in  = e.getSerie('run').getVolume('^vtde1').removeEmpty.toJob;
-vtde_out = addprefixtofilenames(vtde_in,'mean_');
-do_fsl_mean(vtde_in,vtde_out,par);
-
-e.getSerie('run').addVolume('^mean_vtde1.nii','mean_vtde1',1)
-
-
-%% bet (brain extraction)
-
-clear par
-par.run   = 1;
-par.fake  = 0;
-par.sge   = 0;
-par.fsl_output_format = 'NIFTI';
-
-run_dir = e.getSerie('run').removeEmpty.toJob(0);
-
-job = cell(0);
-for iJob = 1 : length(run_dir)
-    job{iJob,1} = sprintf('export FSLOUTPUTTYPE=NIFTI; cd %s; /network/lustre/iss01/cenir/software/irm/fsl5/bin/bet %s %s -R -m -f 0.3',...
-        run_dir{iJob},...
-        'mean_vtde1.nii',...
-        'bet_mean_vtde1.nii');
+if CLUSTER
+    par.run    = 0;
+    par.sge    = 1;
+else
+    par.run    = 1;
+    par.sge    = 0;
 end
-par.jobname = 'fslbet';
-do_cmd_sge(job,par);
 
-e.getSerie('run').addVolume('^bet_mean_vtde1_mask.nii','mask_vtde1',1)
-e.getSerie('run').addVolume('^bet_mean_vtde1.nii'     , 'bet_vtde1',1)
+par.fake       = 0;
+par.redo       = 0;
 
+par.fsl_output_format = 'NIFTI_GZ';
+[job, fmask] = do_fsl_robust_mask_epi(fin, par);
+
+% copy original gz & unzip
+e.gser('run').addVolume('^bet.*vtde1.nii.gz','bet',1);
+e.gser('run').gvol('bet').removeEmpty.unzip_and_keep(par);
+
+% %% temporal mean #obsolete !!!
+% 
+% clear par
+% par.run   = 1;
+% par.fake  = 0;
+% par.sge   = 0;
+% par.fsl_output_format = 'NIFTI';
+% 
+% par.jobname = 'fslmean';
+% vtde_in  = e.getSerie('run').getVolume('^vtde1').removeEmpty.toJob;
+% vtde_out = addprefixtofilenames(vtde_in,'mean_');
+% do_fsl_mean(vtde_in,vtde_out,par);
+% 
+% e.getSerie('run').addVolume('^mean_vtde1.nii','mean_vtde1',1)
+% 
+% 
+% %% bet (brain extraction)
+% 
+% clear par
+% par.run   = 1;
+% par.fake  = 0;
+% par.sge   = 0;
+% par.fsl_output_format = 'NIFTI';
+% 
+% run_dir = e.getSerie('run').removeEmpty.toJob(0);
+% 
+% job = cell(0);
+% for iJob = 1 : length(run_dir)
+%     job{iJob,1} = sprintf('export FSLOUTPUTTYPE=NIFTI; cd %s; /network/lustre/iss01/cenir/software/irm/fsl5/bin/bet %s %s -R -m -f 0.3',...
+%         run_dir{iJob},...
+%         'mean_vtde1.nii',...
+%         'bet_mean_vtde1.nii');
+% end
+% par.jobname = 'fslbet';
+% do_cmd_sge(job,par);
+% 
+% e.getSerie('run').addVolume('^bet_mean_vtde1_mask.nii','mask_vtde1',1)
+% e.getSerie('run').addVolume('^bet_mean_vtde1.nii'     , 'bet_vtde1',1)
+% 
 
 %% Tedana
 
@@ -165,35 +206,38 @@ par.run      = 0;
 
 % Cluster
 if par.sge               % for ICM cluster, run the jobs in paralle
-    par.jobname  = 'job_tedana';
-    par.walltime = '08:00:00';      % HH:MM:SS
-    par.mem      = 16000;           % MB
-    par.sge_queu = 'normal,bigmem'; % use both
+    par.jobname      = 'job_tedana';
+    par.walltime     = '12:00:00';      % HH:MM:SS
+    par.mem          = '16G';           % MB
+    %par.sge_queu = 'normal,bigmem'; % use both
+    par.sge_nb_coeur = 2;
 end
 
 %!source python_path3.6
 %!source activate tedana_0.0.9a1
 
-job_tedana_009a1( meinfo, 'vtd', 'tedana_vtd_mle', 'bet_mean_vtde1_mask.nii', par );
+job_tedana_009a1( meinfo, 'vtd', 'tedana009a1_vtd', 'bet_Tmean_vtde1_mask.nii.gz', par );
 
-
-%return %not sure if necessary
 
 %% tedana report % meinfo is in main_dir and tedana outputs in the subdir of run dir (until change - cannot use tedana_report)
-par.subdir = 'tedana_vtd_mle'
+par.subdir = 'tedana009a1_vtd'
 %tedana_report (main_dir, par); % changement et necessite de meinfo.path
-
-%% unzip the outputs of tedana
 
 %% Add new preprocessed data to exam object : dn_ts_OC.nii 
 
-e.addSerie('ACTIVATION','tedana_vtd','tedana_ACTIVATION',1);
-e.addSerie('RS','tedana_vtd','tedana_RS',1);
-e.getSerie('tedana').addVolume('^dn.*.nii','dn',1);
-e.unzipVolume();
+e.addSerie('ACTIVATION','tedana009.*_vtd','tedana_ACTIVATION',1);
+e.addSerie('RS','tedana009.*_vtd','tedana_RS',1);
+e.getSerie('tedana').addVolume('^ts_OC','ts',1);
+e.getSerie('tedana').addVolume('^dn_ts','dn_ts',1);
 
-[ec_dn, ei_dn] = e.removeIncomplete;
-e = ec_dn;
+% copy original gz & unzip 
+e.getSerie('tedana').getVolume('.*ts').removeEmpty.unzip_and_keep(par)
+
+% unzip the outputs of tedana
+%e.unzipVolume();
+% lines below surely not needed anymore
+%[ec_dn, ei_dn] = e.removeIncomplete;
+%e = ei_dn;
 
 % if all in run directory instead of tedana_vtd_mle
 %e(1).getSerie('run').addVolume('^dn.*.nii$','dn',1);
@@ -204,22 +248,32 @@ clear par
 fanat = e.getSerie('anat').getVolume('^s').getPath;
 
 % Retrocompatibility for SPM:Spatial:Segment options
-par.GM        = [1 1 1 1]; % warped_space_Unmodulated(wp*) / warped_space_modulated(mwp*) / native_space(p*) / native_space_dartel_import(rp*)
-par.WM        = [1 1 1 1];
-par.CSF       = [1 1 1 1];
+par.GM        = [1 0 1 0]; % warped_space_Unmodulated(wp*) / warped_space_modulated(mwp*) / native_space(p*) / native_space_dartel_import(rp*)
+par.WM        = [1 0 1 0];
+par.CSF       = [1 0 1 0];
+par.TPMC      = [0 0 0 0];
+
 par.bias      = [1 1 0] ;  % native normalize dartel     [0 1]; % bias field / bias corrected image
 par.warp      = [1 1]; % warp field native->template / warp field native<-template
 par.label     = [1 1 0];
+par.las       = [0 0 0];
 
 par.jacobian  = 0;         % write jacobian determinant in normalize space
 par.doROI     = 0;
 par.doSurface = 0;
 par.subfolder = 0; % all results in the same subfolder
 
-par.run     = 1;
 par.display = 0;
 par.pct     = 0;
 par.redo    = 0;
+
+if CLUSTER
+    par.run     = 0;
+    par.sge     = 1;
+else
+    par.run     = 1;
+    par.sge     = 0;
+end
 
 j_segment = job_do_segmentCAT12(fanat,par);
 
@@ -227,92 +281,142 @@ j_segment = job_do_segmentCAT12(fanat,par);
 e.getSerie('anat').addVolume('^y'  ,'y' );
 e.getSerie('anat').addVolume('^p0' ,'p0' );
 
-%% Coregister func @ anat
-% correction of any former transformations of ^dn
+%% SPM
+%% Coregister TEDANA outputs to Anat
 
-    par.ask = 0;
-    par.redo = 1;
-    par.pct = 0;
-    
-    if par.redo==1
-        
-        e.getSerie('tedana').addVolume('^ts','ts',1);
-        e.unzipVolume;
-        e.getSerie('tedana').addVolume('^ts.*.nii$','ts',1);
-        e.getSerie('tedana').getVolume('ts').path
-        
-        %% for ACTIVATION vols
-        origin = e.getSerie('tedana_ACTIVATION').getVolume('^ts').toJob(0);
-        influencer = e.getSerie('run_ACTIVATION').getVolume('^bet_vtde1').toJob(0);
-        follower = e.getSerie('tedana_ACTIVATION').getVolume('^dn').toJob(0);
-                   
-        do_fsl_copy_header(influencer, origin,      par);
-        do_fsl_copy_header(follower,   influencer,  par);
-        
-
-    %% for RS vols
-        origin = e.getSerie('tedana_RS').getVolume('^ts').toJob(0);
-        influencer = e.getSerie('run_RS').getVolume('^bet_vtde1').toJob(0);
-        follower = e.getSerie('tedana_RS').getVolume('^dn').toJob(0);
-       
-        par.ask = 0;
-        par.pct = 0;
-        do_fsl_copy_header(influencer, origin,      par);
-        do_fsl_copy_header(follower,   influencer,  par);
-    
-    end
-
-%% Coregistration of both runs
+clear par
+if CLUSTER
+    par.run = 0;
+    par.sge = 1;
+else
+    par.run = 1;
+    par.sge = 0;
+end
 
 % ACTIVATION
-    clear par
-    par.type   = 'estimate';
-    par.interp = 1;
-    par.prefix = 'r';
-    par.sge    = 0;
-    par.redo   = 0;
-    par.run    = 1;
-    par.display= 0;
+clear par
+par.type   = 'estimate';
+par.interp = 1;
+par.prefix = 'r';
+par.redo   = 0;
+par.display= 0;
+par.cost_fun = 'ncc';
 
-    ref = e.getSerie('anat').getVolume('^p0').toJob(0);
-    src = e.getSerie('run_ACTIVATION').getVolume('^bet_vtde1').toJob(0);
-    oth = e.getSerie('tedana_ACTIVATION').getVolume('^dn').toJob(0);
+tmp_exam_a = [e.gser('run_ACTIVATION').removeEmpty.exam];
+ref = tmp_exam_a.gser('anat').getVolume('^p0');
+src = e.getSerie('run_ACTIVATION').removeEmpty.getVolume('bet');
+oth = e.getSerie('tedana_ACTIVATION').removeEmpty.getVolume('.*ts');
 
-        % include the skip option (?)
+%keep in mind this option if emergency to get the precedent header
+%do_fsl_copy_header(influencer, origin,      par);
+%do_fsl_copy_header(follower,   influencer,  par);
 
-    job_coregister(char(src),char(ref),char(oth),par)
-    
+
+job_coregister(src,ref,oth,par);
+   
 % RS
-    ref = e.getSerie('anat').getVolume('^p0').toJob(0);
-    src = e.getSerie('run_RS').getVolume('^bet_vtde1').toJob(0);
-    oth = e.getSerie('tedana_RS').getVolume('^dn').toJob(0);
+tmp_exam_rs = [e.getSerie('run_RS').removeEmpty.exam];
+ref = tmp_exam_rs.getSerie('anat').getVolume('^p0');
+src = e.getSerie('run_RS').removeEmpty.getVolume('bet');
+oth = e.getSerie('tedana_RS').removeEmpty.getVolume('.*ts');
 
-    % include the skip option
-
-    job_coregister(char(src),char(ref),char(oth),par)
+job_coregister(src,ref,oth,par);
     
-    %% Normalize
+%% Normalize TEDANA outputs
 
-    clear par
-    par.redo    = 0;
-    par.sge     = 0;
-    par.run     = 1;
-    par.display = 0;
-    par.vox     = [2.5 2.5 2.5];
-    par.jobname = 'spm_apply_norm';
+clear par
+par.redo         = 0;
+if CLUSTER
+    par.sge      = 1;
+    par.run      = 0;
+else
+    par.sge      = 0;
+    par.run      = 1;
+end
+par.auto_add_obj = 0;
+par.display      = 0;
+par.vox          = [2.5 2.5 2.5]; % important to keep original EPI vox size
+par.jobname      = 'spm_normalize_epi';
 
-    warp_field = e.getSerie('anat').getVolume('^y');
 % ACTIVATION
-    img = e.getSerie('tedana_ACTIVATION').getVolume('^dn');
-    job_apply_normalize(warp_field,img, par)
+img_a = e.getSerie('tedana_ACTIVATION').getVolume('.*ts').removeEmpty;
+tmp_exam = [img_a.exam];
+warp_field = tmp_exam.getSerie('anat').getVolume('^y');
+job_apply_normalize(warp_field, img_a, par);
+
 % RS
-    img = e.getSerie('tedana_RS').getVolume('^dn');
-    job_apply_normalize(warp_field,img, par)
+img_rs = e.getSerie('tedana_RS').getVolume('.*ts').removeEmpty;
+tmp_exam_rs = [img_rs.exam];
+warp_field = tmp_exam_rs.getSerie('anat').getVolume('^y');
+job_apply_normalize(warp_field, img_rs, par);
 
-    e.getSerie('tedana').addVolume('^wdn','wdn',1);
-    e.explore
+%% Normalize the Tmean mask used later for PhysIO Noise ROI
+par.auto_add_obj = 1;
+img = e.gser('run').removeEmpty.gvol('bet');
+tmp_exam = [img.exam];
+y = tmp_exam.gser('anat').gvol('^y');
 
-    save('e','e')
+par.jobname = 'spm_normalize_meanepi';
+job_apply_normalize(y, img, par);
+
+
+%% add outputs to exam object
+e.getSerie('tedana').addVolume('^wts','wts',1);
+e.getSerie('tedana').addVolume('^wdn','wdn',1);
+e.getSerie('run').addVolume('^wbet','wbet',1);
+
+%% Smooth TEDANA outputs
+
+clear par
+par.redo         = 0;
+if CLUSTER
+    par.run      = 0;
+    par.sge      = 1;
+else
+    par.run      = 1;
+    par.sge      = 0;
+end
+par.auto_add_obj = 0;
+par.smooth       = [5 5 5];
+par.prefix       = 's5';
+par.jobname      = 'spm_smooth_5';
+
+img = e.gser('tedana').gvol('^w').removeEmpty;
+
+job_smooth(img, par);
+
+%% save smoothed outputs
+e.gser('tedana').addVolume('^s5wts_OC.nii','s5wts',1);
+e.gser('tedana').addVolume('^s5wdn_ts_OC.nii','s5wdn',1);
+
+return
+
+%% coregister WM & CSF on functional (warped_mean_mask)
+% used for TAPAS: PhysIO
+
+clear par
+if CLUSTER
+    par.run = 0;
+    par.sge = 1;
+else
+    par.run = 1;
+    par.sge = 0;
+end
+par.type    = 'estimate_and_write';
+
+ref = e.getSerie('run');
+ref = ref(:,1).getVolume('wbet');
+e.getSerie('anat').addVolume('^wp2','wp2',1);
+e.getSerie('anat').addVolume('^wp3','wp3',1);
+src = e.getSerie('anat').getVolume('^wp2');
+oth = e.getSerie('anat').getVolume('^wp3');
+
+job_coregister(src, ref, oth, par);
+
+%% Check & Save
+e.explore
+cd main_dir
+save('e','e')
     
-    %%
+%%
 
