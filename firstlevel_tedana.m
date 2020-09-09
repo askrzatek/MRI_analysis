c5f95093810da094aca83215917b6930926b81b2
askrzatek <skrzatek.a@gmail.com>
2020-09-09 17:01:08 +0200

    add changes related to 3 different denoising processes - 3 models instead of one, decomment of single-figure-display-save, symbolic-link loop to improve, main_dir changed

--- firstlevel_tedana.m
+++ firstlevel_tedana.m
@@ -5,15 +5,18 @@ clc
 
 addpath /home/anna.skrzatek/data/StimTemplate
 cd      /home/anna.skrzatek/data
-%load e % why doesn't the e-object work?!
 
-main_dir = fullfile(pwd,'nifti');
-stim_dir = fullfile(pwd,'behav');
+%main_dir = fullfile(pwd,'nifti');
+stim_dir = fullfile(pwd,'/nifti_test/behav_test');
+main_dir = fullfile(pwd,'/nifti_test/ben');
 
 %model_name = 'model_meica';
-model_name = 'model_tedana';
+%model_name = 'model_tedana';
+model_name = {'model_tedana','model_ts_tapas', 'model_dn_tapas'};
 
 %% fetch dirs
+cd (main_dir)
+load e % why doesn't the e-object work?!
 
 dir_func  = e.getSerie('run_ACTIVATION') .toJob;
 dir_anat = e.getSerie('anat').toJob(0);
@@ -22,41 +25,108 @@ par.fake = 0;
 par.redo = 0;
 par.verbose = 2;
 
-model_dir = e.mkdir(model_name);
+model_outdir1 = e.mkdir(model_name{1});
+model_outdir2 = e.mkdir(model_name{2});
+model_outdir3 = e.mkdir(model_name{3});
 
 
-%% Fetch onset
+%% Fetch onset & mask
 
 e.getSerie('run_ACTIVATION').addStim(stim_dir, 'MRI_run\d{2}_SPM.mat', 'run', 1 )
 stim_files = e.getSerie('run_ACTIVATION').getStim.toJob(0);
+e.getSerie('run_ACTIVATION').addVolume('mask.nii','mask',1)
 
 % e.explore
 
-%% Make symbolic links from tedana_vtd_mle dir to run dir based on job_meica_afni symbolic link creation
-par.subdir        = 'tedana_vtd_mle';
-par.warp_file_reg = 'wdn';
+%% Make symbolic links from tedana_vtd_mle dir to run dir based on job_meica_afni symbolic link creation 
+%par.subdir        = 'tedana_vtd_mle';
+par.subdir        = 'tedana009_vtd';
 
+par.warp_file_reg = '^wdn';
 job_symbolic_child_to_parent(dir_func, par);
+
+par.warp_file_reg = 's5wts';
+job_symbolic_child_to_parent(dir_func, par);
+
+par.warp_file_reg = 's5wdn';
+job_symbolic_child_to_parent(dir_func, par);
+
+%% make a symbolic link of rp_spm.txt and of multiple_regressors to dir_func
+par.subdir = 'wts';
+par.regfile_regex = 'multiple_regressors.txt';
+for i= 1:length(e)
+    wd = e(i).getSerie('run_ACTIVATION').path;
+    reg_dir = char(get_subdir_regex(wd, par.subdir));
+    A_src = fullfile(reg_dir, par.regfile_regex);
+    regfile_out = sprintf('%s_%s',par.subdir,par.regfile_regex);
+    A_dst = fullfile(wd, regfile_out);
+    
+    par.redo = 0;
+    par.verbose = 2;
+    par.run = 0;
+    par.run = 1;
+    par.jobname = sprintf('job_symbolic_link');
+    %par.jobname = sprintf('%s_%s_%s', 'job_symbolic_link', wd(end-46:end-16), par.subdir);
+    [job_session(i)] = r_movefile(A_src, A_dst, 'linkn', par);
+    job = [job_session];
+    
+end
+%job = do_cmd_sge(job, par);
 
 
 %% Job define model
+clear par
 par.sge = 0;
-par.run = 1;
-%par.pct = 0;
+par.run = 0;
+par.display = 0;
+%par.mask = e.getSerie('run_ACTIVATION').getVolume('mask').toJob;
+par.rp = 1;
+par.mask_thr = 0.1;
+%par.mask_thr = 0.07;
 % par.TR = 1.6;
 
+%% Classic tedana
 par.file_reg = '^wdn';
-% par.rp = 1;
-par.mask_thr = 0.07;
+par.rp_regex = 'rp.*txt';
+job1 = job_first_level_specify(dir_func, model_outdir1, stim_files, par);
 
-%% Define model
+%% ts TAPAS
+par.file_reg = '^s5wts';
+par.rp_regex = 'wts_multiple_regressors.txt';
+job2 = job_first_level_specify(dir_func, model_outdir2, stim_files, par);
 
-job_first_level_specify(dir_func,model_dir,stim_files,par)
+%% tedana + TAPAS
 
+par.file_reg = '^s5wdn';
+
+par.rp_regex = 'wdn_multiple_regressors.txt';
+job3 = job_first_level_specify(dir_func, model_outdir3, stim_files, par);
+
+jobs = [job1 job2 job3 ];
+
+par.sge = 1;
+par.run = 0;
+par.display = 0;
+par.jobname = 'spm_glm_def';
+
+job_ending_rountines(jobs,[],par);
+
+return
 
 %% Estimate
 
-fspm = e.addModel(model_name,model_name);
+e.addModel(model_name{1},model_name{1});
+e.addModel(model_name{2},model_name{2});
+e.addModel(model_name{3},model_name{3});
+
+save ('e','e')
+
+par.run = 0;
+par.sge = 1;
+par.display = 0;
+par.jobname = 'spm_glm_est'
+fspm = e.getModel(cellstr2regex({model_name{1}, model_name{2}, model_name{3}},1)).removeEmpty.toJob;
+
 job_first_level_estimate(fspm,par)
 
 
@@ -182,17 +252,21 @@ clear par
 par.sge = 0;
 par.run = 1;
 par.display = 0;
+par.jobname = 'spm_glm_con';
 
 % par.sessrep = 'both';
 par.sessrep = 'none';
 
 par.delete_previous = 1;
 par.report          = 0;
+
+fspm = e.getModel(cellstr2regex({model_name{1}, model_name{2}, model_name{3}},1)).removeEmpty.toJob;
+
 job_first_level_contrast(fspm,contrast,par);
 
 %% add the model and the contrasts to the e object
 
-e.addSerie('^model_tedana','model',1);
+%e.addSerie('^model','model',3);
 e.getSerie('model').addVolume('^SPM','m',1);
 e.getSerie('model').addVolume('^spm[TF]_\d{4}','con',length(contrast.names));
 
@@ -220,25 +294,27 @@ e.getSerie('model').addVolume('^spm[TF]_\d{4}','con',length(contrast.names));
 %save e
 
 %% Create figures
+clear par
 
-%fsub = e.gpath;
-%Coordlist.values = cat(2,[0.0; 0.0; 0.0], [34.0; -24.0; 68.0], [-34.0; -24.0; 68.0], [-4.0; -48.0; -24.0], [0.0; -24.0; 10.0]); % EXAMPLE
-%Coordlist.names = {'centre'; 'RIGHT SM'; 'LEFT SM'; 'CEREBELLUM'; 'MOTOR BI'}; % EXAMPLE
+fsub = e.gpath;
+Coordlist.values = cat(2,[0.0; 0.0; 0.0], [34.0; -24.0; 68.0], [-34.0; -24.0; 68.0], [-4.0; -48.0; -24.0], [0.0; -24.0; 10.0]); % EXAMPLE
+Coordlist.names = {'centre'; 'RIGHT SM'; 'LEFT SM'; 'CEREBELLUM'; 'MOTOR BI'}; % EXAMPLE
 
-%par.subdir       = 'model_tedana';
-%par.anat_dir_reg = 'S\d{2}_t1mpr_S256_0_8iso_p2$'
+par.subdir       = 'model_dn_tapas';
+par.anat_dir_reg = 'S\d{2}_t1mpr_S256_0_8iso_p2$'
+par.extent       = 20;
 
-%conlist = {'F-all','REAL_Left','REAL_Right','IMAGINARY_Left','IMAGINARY_Right','REAL_Left - Rest','REAL_Right - Rest','IMAGINARY_Left - Rest','IMAGINARY_Right - Rest'};
+conlist = {'F-all','REAL_Left','REAL_Right','IMAGINARY_Left','IMAGINARY_Right','REAL_Left - Rest','REAL_Right - Rest','IMAGINARY_Left - Rest','IMAGINARY_Right - Rest'};
 addpath /home/anna.skrzatek/matvol/SPM/
-%for icon = 1 : length(conlist)
-%    par.conname  = conlist{icon};
-%    job_spm_single_results_display(fsub, Coordlist, par);
-%end
+for icon = 1 : length(conlist)
+   par.conname  = conlist{icon};
+   job_spm_single_results_display(fsub, Coordlist, par);
+end
 
-%close all
+close all
 
 %%script_report_edit(e);
 
 %% Display
 
-%%e.getOne.getModel(model_name).show
+%%e.getOne.getModel(model_name{2}).show
