7b73cf78efb20e51bfa1b7a7d727b06b60c8162e
askrzatek <skrzatek.a@gmail.com>
2021-05-21 16:20:49 +0200

    version mise a jour et terminee pour les V1 et V2 separee - il faut encore rajouter la partie delta optionnelle

--- RS_secondlevel_ROI_par_group.m
+++ RS_secondlevel_ROI_par_group.m
@@ -4,9 +4,11 @@ clc
 clear all
 
 main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek','/nifti_test');
+stat_dir = fullfile(main_dir,'resliced_secondlevel_RS');
+
 cd (main_dir)
 
-Stat = exam();
+Stat = exam(stat_dir,'PARK');
 
 InputRS   = fullfile(main_dir,'firstlevel_RS'); % newly created - PCC models missing
 
@@ -61,5 +63,82 @@ for iC = 1 : length(Conditions)
     end
 end
 outdirs = Stat.getSerie('.*') .toJob; % 7 x 1 x 108 cells
+groups  = {gRS_a, gRS_c};
 
 addpath /home/anna.skrzatek/MRI_analysis/
+
+par.run = 0;
+par.sge = 1;
+par.jobname = 'job_RS_secondlevel_auto';
+
+secondlevel_RS_matlabbatch(groups,outdirs,par)
+
+%% models estimate
+
+clear par
+cd (main_dir)
+
+for iout = 1 : length(outdirs)
+    
+    fspm = addsuffixtofilenames(outdirs{iout}, 'SPM.mat');
+
+    par.run = 1;
+    %par.sge = 1;
+    par.sge_queu = 'normal,bigmem';
+    par.jobname  = 'spm_secondlevel_RS_est';
+    job_first_level_estimate(fspm,par)
+    
+end
+
+%% Contrast creation for each SPM.mat
+
+% t-statistics
+    Main = 1;
+
+for iout = 1 : length(outdirs)
+    %fspm = addsuffixtofilenames(outdirs{iout},'SPM.mat');
+    modest = addsuffixtofilenames(outdirs{iout},'SPM.mat');
+    for iroi = 1 : length(modest)
+        parts = strsplit(char(modest(iroi)), '/');
+        roilabel = parts{end-1};   
+    
+        %% Contrast names
+        contrast_t.names = {
+            sprintf('Connectivity %s',roilabel)
+            }';
+
+        %% Contrast values
+        contrast_t.values = {
+            Main
+            }';
+
+        %% Contrast type
+        contrast_t.types = cat(1,repmat({'T'},[1 length(contrast_t.names)]));
+
+        contrast.names  = [contrast_t.names];
+        contrast.values = [contrast_t.values];
+        contrast.types  = [contrast_t.types];
+
+        %% Contrast : write
+        clear par
+
+        par.sge = 0;
+        par.run = 1;
+        par.display = 0;
+        par.jobname = sprintf('spm_write_%s_con',roilabel);
+
+        % par.sessrep = 'both';
+        par.sessrep = 'none';
+
+        par.delete_previous = 1;
+        par.report          = 0;
+
+        job_first_level_contrast(modest(iroi),contrast,par);
+        
+%         Stat(iout).getSerie(roilabel).addVolume('spmT_0001','main',1)
+        
+%         mainef = Stat(iout).getSerie(roilabel).getVolume('main') .toJob
+        
+        mask{iroi} = cellstr(fullfile(outdirs{iout}{iroi},'mask.nii'));
+    end
+end
--- secondlevel_RS_matlabbatch.m
+++ secondlevel_RS_matlabbatch.m
@@ -16,6 +16,7 @@ par = complet_struct(par, defpar);
 %%
 ijob = 1;
 for igroup = 1 : length(groups)
+    for icon = 1 : length(groups{igroup})
         % jobs{ijob}.spm.stats.factorial_design.dir = {'/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/secondlevel_RS_mar21/RS_PARKGAME_c/SMA_R_c/V2'};
         % jobs{ijob}.spm.stats.factorial_design.des.t1.scans = {
         %                                                           '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/2018_10_26_PARKGAMEII_003_SM_26_10_2018_V2_c/S05_RS/model/Modele_VOI__Supp_Motor_Area_R/con_0001.nii,1'
@@ -25,8 +26,8 @@ for igroup = 1 : length(groups)
         %                                                           '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/2020_07_27_PARKGAMEII_044_CK_27_07_2020_V2_c/S06_RS/model/Modele_VOI__Supp_Motor_Area_R/con_0001.nii,1'
         %                                                           '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/2020_11_18_PARKGAMEII_047_BF_18_11_2020_V2_c/S06_RS/model/Modele_VOI__Supp_Motor_Area_R/con_0001.nii,1'
         %                                                           };
-        jobs{ijob}.spm.stats.factorial_design.dir = outdirs(igroup);
-        jobs{ijob}.spm.stats.factorial_design.des.t1.scans = spm_select('expand',groups{igroup});
+        jobs{ijob}.spm.stats.factorial_design.dir = outdirs{igroup}(icon);
+        jobs{ijob}.spm.stats.factorial_design.des.t1.scans = spm_select('expand',groups{igroup}{icon});
         jobs{ijob}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
         jobs{ijob}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
         jobs{ijob}.spm.stats.factorial_design.masking.tm.tm_none = 1;
@@ -40,6 +41,7 @@ for igroup = 1 : length(groups)
 
         ijob = ijob +1;
     end
+end
 
 [ jobs ] = job_ending_rountines( jobs, skip, par );
 
