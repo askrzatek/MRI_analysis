4b22621a47ccab5c87e48bd1bcabfc5e4d43fc5b
askrzatek <skrzatek.a@gmail.com>
2021-12-07 18:17:05 +0100

    adding the part of analysis for RS ANOVA SxG

--- RS_secondlevel_ROI_par_group.m
+++ RS_secondlevel_ROI_par_group.m
@@ -5,6 +5,7 @@ clear all
 
 main_dir = fullfile('/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek','/nifti_test');
 stat_dir = fullfile(main_dir,'full_secondlevel_RS');
+A_dir = fullfile(main_dir,'resliced_RS_ANOVA_V1V2');
 
 cd (main_dir)
 
@@ -32,6 +33,7 @@ ROIs = {'Caudate_L','Caudate_R','Cereb3_L','Cereb3_R','Cereb4_5_L','Cereb4_5_R',
 %ROIs = {'Cereb3_L','Cereb3_R','Cereb4_5_L','Cereb4_5_R','Cereb6_L','Cereb6_R','Cereb7b_L','Cereb7b_R','Cereb8_L','Cereb8_R','Cereb9_L','Cereb9_R','Cereb10_L','Cereb10_R','Cereb_Crus1_L','Cereb_Crus1_R','Cereb_Crus2_L','Cereb_Crus2_R'};
 Conditions  = ROIs;
 Sessions    = {'V1','V2'}%,'V2-V1'};
+A_Sessions  = {'V2_V1'}; % pour ANOVA
 
 for iR = 1 : length(ROIs)
     for iS = 1 : length(Sessions)
@@ -49,11 +51,14 @@ for iR = 1 : length(ROIs)
         RSObj_c.getSerie(sprintf('%s_%s',ROIs{iR},Sessions{iS})).addVolume('con.*01',sprintf('%s_%s',ROIs{iR},Sessions{iS}))
         
     end
+    mkdir(A_dir,sprintf('%s_V2_V1',ROIs{iR}))
+    
     % FIRSTLEVEL for ALL PARKs add volumes for each ROI's contrast
      
      RS_V1_all.addSerie(sprintf('%s_V1$',ROIs{iR}),sprintf('%s_V1',ROIs{iR}),1);
      RS_V1_all.getSerie(sprintf('%s_V1$',ROIs{iR})).addVolume('con.*01',sprintf('%s_V1_con$',ROIs{iR}))
 end
+
 
 clear RS_allcell gRS_all
 
@@ -84,6 +89,7 @@ end
 %outdirs = Stat.getSerie('Cereb.*V1') .toJob; % 7 x 1 x 108 cells
 outdirs = Stat.getSerie('.*V') .toJob; % 7 x 1 x 108 cells
 outdirs = outdirs(2:3);
+A_outdir = exam(A_dir,'V2_V1').toJob;
 groups  = {gRS_a, gRS_c};
 outdirs_V1 = Stat(1).getSerie('.*V1') .toJob; % 7 x 1 x 108 cells
 group_V1 = gRS_all;
@@ -224,6 +230,7 @@ for iout = 1 : length(outdirs_V1)
     
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Contrast creation for each SPM.mat
 
 % t-statistics
@@ -279,8 +286,12 @@ for iout = 1 : length(outdirs)
     end
 end
 
-    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
 %% V1
+% t-statistics
+    Main = 1;
+
+%%
 
 for iout = 1 : length(outdirs_V1)
     %fspm = addsuffixtofilenames(outdirs{iout},'SPM.mat');
@@ -553,4 +564,164 @@ for iout = 1 : length(outdirs)
         mask{iroi} = cellstr(fullfile(outdirs{iout}{iroi},'mask.nii'));
     end
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% full factorial ANOVA model definition
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n = 1;
+for iC = 1 : length(Conditions)
+    RS_a_V1{n}   = RSObj_a.getSerie(sprintf('%s_V1',Conditions{iC})).getVolume(sprintf('%s_V1',Conditions{iC})) %.toJob;
+    gRS_a_V1{n} = cellstr(RS_a_V1{n}(:) .toJob)            
+    RS_c_V1{n}  = RSObj_c.getSerie(sprintf('%s_V1',Conditions{iC})).getVolume(sprintf('%s_V1',Conditions{iC})) %.toJob;
+    gRS_c_V1{n} = cellstr(RS_c_V1{n}(:) .toJob)
+
+    RS_a_V2{n}   = RSObj_a.getSerie(sprintf('%s_V2',Conditions{iC})).getVolume(sprintf('%s_V2',Conditions{iC})) %.toJob;
+    gRS_a_V2{n} = cellstr(RS_a_V2{n}(:) .toJob)            
+    RS_c_V2{n}  = RSObj_c.getSerie(sprintf('%s_V2',Conditions{iC})).getVolume(sprintf('%s_V2',Conditions{iC})) %.toJob;
+    gRS_c_V2{n} = cellstr(RS_c_V2{n}(:) .toJob)
+    
+    n = n+1;
+end
+
+% define the volumes per group
+A_groups  = {gRS_a_V1, gRS_a_V2; gRS_c_V1, gRS_c_V2}; % A_groups{1,:} : 2 groupes en V1 ; A_groups{:,1} : 2 visites de Kinect
+
+% define models
+cd (main_dir)
+addpath /home/anna.skrzatek/MRI_analysis/
+
+par.run = 0;
+par.sge = 1;
+par.jobname = 'job_RS_ANOVA_full_auto_covars';
+par.con_auto = 0;
+
+ANOVA_RS_matlabbatch(A_groups,A_outdir,covars,par)
+
+%% models estimate
+clear par
+cd (main_dir)
+
+for iout = 1 : length(A_outdir)
+    
+    fspm = addsuffixtofilenames(A_outdir(iout), 'SPM.mat');
+
+    par.run = 0;
+    par.sge = 1;
+    par.sge_queu = 'normal,bigmem';
+    par.jobname  = 'spm_ANOVA_RS_est';
+    job_first_level_estimate(fspm,par)
+    
+end
+
+%% Contrast creation for each SPM.mat
+
+% F-statistics
+    Average_condition_effect         = [ 1 1  1  1 0 0];
+    Main_Session_effect              = [ 1 1 -1 -1 0 0];
+    Main_Group_effect                = [ 1 -1 1 -1 0 0];
+    Main_Interaction_SxG_effect      = [ 1 -1 -1 1 0 0];
+    
+% t-statistics
+    Positive_condition_eff           = [1 1 1 1 0 0];
+    Negative_condition_eff           = [-1 -1 -1 -1 0 0];
+    Positive_Training_eff            = [-1 -1 1 1 0 0];
+    Negative_Training_eff            = [1 1 -1 -1 0 0];
+    Positive_Kinect_eff              = [1 -1 1 -1 0 0];
+    Positive_Ordi_eff                = [-1 1 -1 1 0 0];
+    Positive_Interaction_eff         = [1 -1 -1 1 0 0];
+    Negative_Interaction_eff         = [-1 1 1 -1 0 0];
+
+%% for all ROIs
+
+for iout = 1 : length(A_outdir)
+    %fspm = addsuffixtofilenames(A_outdir{iout},'SPM.mat');
+    modest = addsuffixtofilenames(A_outdir{iout},'SPM.mat');
+    parts = strsplit(modest, '/');
+    roilabel = parts{end-1};   
+
+    %% Contrast names
+    contrast_F.names = {
+        sprintf('Average condition effect %s',roilabel)
+        sprintf('Main session effect %s',roilabel)
+        sprintf('Main group effect %s',roilabel)
+        sprintf('Main SxG Interaction effect %s',roilabel)
+        };
+
+    contrast_t.names = {
+        sprintf('Positive Condition effect %s',roilabel)
+        sprintf('Negative Condition effect %s',roilabel)
+        sprintf('Positive Training effect %s',roilabel)
+        sprintf('Negative Training effect %s',roilabel)
+        sprintf('Positive Kinect effect %s',roilabel)
+        sprintf('Positive Ordi effect %s',roilabel)
+        sprintf('Positive Interaction effect %s',roilabel)
+        sprintf('Negative Interaction effect %s',roilabel)
+        }';
+
+    %% Contrast values
+    contrast_F.values = {
+        Average_condition_effect
+        Main_Session_effect
+        Main_Group_effect
+        Main_Interaction_SxG_effect
+        }';
+
+    contrast_t.values = {
+        Positive_condition_eff
+        Negative_condition_eff
+        Positive_Training_eff
+        Negative_Training_eff
+        Positive_Kinect_eff
+        Positive_Ordi_eff
+        Positive_Interaction_eff
+        Negative_Interaction_eff
+        }';
+
+    %% Contrast type
+    contrast_F.types = cat(1,repmat({'F'},[1 length(contrast_F.names)]));
+    contrast_t.types = cat(1,repmat({'T'},[1 length(contrast_t.names)]));
+
+%    contrast.names  = [contrast_F.names contrast_t.names];
+%    contrast.values = [contrast_F.values contrast_t.values];
+%    contrast.types  = [contrast_F.types contrast_t.types];
+
+    %% F-Contrast : write
+    clear par
+
+    par.sge = 0;
+    par.run = 1;
+    par.display = 0;
+    par.jobname = sprintf('spm_write_%s_F_con',roilabel);
+
+    % par.sessrep = 'both';
+    par.sessrep = 'none';
+
+    par.delete_previous = 1;
+    par.report          = 0;
+
+    contrast.names  = [contrast_F.names];
+    contrast.values = [contrast_F.values];
+    contrast.types  = [contrast_F.types];
+
+    job_first_level_contrast({modest},contrast,par);
+
+    %% t-Contrast : write
+
+    par.jobname = sprintf('spm_write_%s_t_con',roilabel);
+    par.delete_previous = 0;
+    
+    contrast.names  = [contrast_t.names];
+    contrast.values = [contrast_t.values];
+    contrast.types  = [contrast_t.types];
+    
+    job_first_level_contrast({modest},contrast,par);
+
+%         Stat(iout).getSerie(roilabel).addVolume('spmT_0001','main',1)
+
+%         mainef = Stat(iout).getSerie(roilabel).getVolume('main') .toJob
+
+%    mask{iroi} = cellstr(fullfile(A_outdir{iout}{iroi},'mask.nii'));
+end
+
 
