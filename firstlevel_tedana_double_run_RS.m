a3032850486f43f8130fcf315d16b9173ae897bf
askrzatek <skrzatek.a@gmail.com>
2021-12-08 17:17:08 +0100

    adapted and tested version : although con_0001.nii is not the file needed in the model definition. We need to do the same with the firstlevel scripts for RS data

--- firstlevel_tedana_double_run_RS.m
+++ firstlevel_tedana_double_run_RS.m
@@ -13,20 +13,26 @@ cd      /home/anna.skrzatek/data
 main_dir = fullfile(pwd,'/nifti_test');
 
 model_name = {'full_RS_sts_tapas_doublerun_resliced'};%, 'smodel_ts_tapas', 'smodel_dn_tapas'};
+mkdir(main_dir,model_name);
+double_model_dir = fullfile(main_dir,model_name);
 
 %% fetch Input dirs
 RSinput_dir = fullfile(main_dir,'/firstlevel_RS')
 cd (RSinput_dir)
 
-patient_regex = {'PARKGAMEII.*NB.*_a','PARKGAMEII.*BM.*_a','PARKGAMEII.*SM.*_c','PARKGAMEII.*SD.*_a','PARKGAMEII.*JR.*_a','PARKGAMEII.*LJ.*_c','PARKGAMEII.*CA.*_a','PARKGAMEII.*PC.*_c','PARKGAMEII.*DD.*','PARKGAMEII.*KM.*_a','PARKGAMEII.*PD.*_a','PARKGAMEII.*CK.*_c','PARKGAMEII.*BF.*_c','PARKGAMEII.*SB.*_a','PARKGAMEII.*HJ.*_c'}; %,'PARKGAMEII.*LM.*_c'};
+patient_regex = {'PARKGAMEII.*NB.*_a','PARKGAMEII.*BM.*_a','PARKGAMEII.*SM.*_c','PARKGAMEII.*SD.*_a','PARKGAMEII.*JR.*_a','PARKGAMEII.*LJ.*_c','PARKGAMEII.*CA.*_a','PARKGAMEII.*PC.*_c','PARKGAMEII.*DD.*','PARKGAMEII.*KM.*_a','PARKGAMEII.*PD.*_a','PARKGAMEII.*CK.*_c','PARKGAMEII.*BF.*_c','PARKGAMEII.*SB.*_a','PARKGAMEII_052.*HJ.*_c'}; %,'PARKGAMEII.*LM.*_c'};
 %patient_regex = {'PARKGAMEII_001_NB_a','PARKGAMEII_002_BM_a','PARKGAMEII_003_SM_c','PARKGAMEII_007_SD_a','PARKGAMEII_008_JR_a','PARKGAMEII_023_LJ_c','PARKGAMEII_025_CA_a','PARKGAMEII_028_PC_c','PARKGAMEII_033_DD','PARKGAMEII_039_KM_a','PARKGAMEII_043_PD_a','PARKGAMEII_044_CK_c','PARKGAMEII_047_BF_c','PARKGAMEII_048_SB_a','PARKGAMEII_052_HJ_c'}; 
 % patient_regex = {'PARKGAMEII.*009_HJ','PARKGAMEII.*013_RP','PARKGAMEII.*027_OR','PARKGAMEII.*046_HJ','PARKGAMEII.*053_LM}; %exclu
 
 ROIs = {'Caudate_L','Caudate_R','Cereb3_L','Cereb3_R','Cereb4_5_L','Cereb4_5_R','Cereb6_L','Cereb6_R','Cereb7b_L','Cereb7b_R','Cereb8_L','Cereb8_R','Cereb9_L','Cereb9_R','Cereb10_L','Cereb10_R','Cereb_Crus1_L','Cereb_Crus1_R','Cereb_Crus2_L','Cereb_Crus2_R','Cuneus_L','Cuneus_R','Insula_L','Insula_R','Pallidum_L','Pallidum_R','Parietal_Inf_L','Parietal_Inf_R','Parietal_Sup_L','Parietal_Sup_R','PCC','Postcentral_L','Postcentral_R','PPN_L','PPN_R','Precentral_L','Precentral_R','Precuneus_L','Precuneus_R','Putamen_L','Putamen_R','SMA_Ant_L','SMA_Ant_R','SMA_Post_L','SMA_Post_R','Thalamus_L','Thalamus_R','Vermis1_2','Vermis3','Vermis4_5','Vermis6','Vermis7','Vermis8','Vermis9','Vermis10'};
 %    ROIs = {'SMA_face_L','SMA_face_R','SMA_foot_L','SMA_foot_R','SMA_hand_L','SMA_hand_R'};
-    
+
+% patient_regex = {'PARKGAMEII.*NB.*_a'}
+% ROIs = {'Caudate_L'};
+
 dirFonc = cell(length(patient_regex),length(ROIs));
 
+i = 1;
 for ip = 1 : length(patient_regex)    
     clear esuj
     esuj = exam(RSinput_dir,patient_regex{ip});
@@ -47,11 +53,12 @@ for ip = 1 : length(patient_regex)
         par.regfile_regex = 'multiple_regressors.txt';
         regfile_out = sprintf('%s_%s',par.subdir,par.regfile_regex);
 
-        A_src1 = fullfile(reg_src_dir1, par.regfile_regex);
-        A_dst1 = fullfile(dirFonc{ip,ir,1}, regfile_out);
-        A_src2 = fullfile(reg_src_dir2, par.regfile_regex);
-        A_dst2 = fullfile(dirFonc{ip,ir,2}, regfile_out);
-    
+        A_src1 = fullfile(reg_src_dir1{:}, par.regfile_regex);
+        A_src2 = fullfile(reg_src_dir2{:}, par.regfile_regex);
+        A_dst = fullfile(dirFonc{ip,ir}, regfile_out);
+        A_dst1 = A_dst{1};
+        A_dst2 = A_dst{2};
+
         par.redo = 0;
         par.verbose = 2;
         par.run = 0;
@@ -68,11 +75,12 @@ for ip = 1 : length(patient_regex)
         clear par
         par.regfile_regex = 'wbet_Tmean_vtde1_mask.nii';
 
-        A_src1 = fullfile(reg_src_dir1, par.regfile_regex);
-        A_dst1 = fullfile(dirFonc{ip,ir,1}, par.regfile_regex);
-        A_src2 = fullfile(reg_src_dir2, par.regfile_regex);
-        A_dst2 = fullfile(dirFonc{ip,ir,2}, regfile_out);
-    
+        A_src1 = fullfile(reg_src_dir1{:}, par.regfile_regex);
+        A_src2 = fullfile(reg_src_dir2{:}, par.regfile_regex);
+        A_dst = fullfile(dirFonc{ip,ir}, regfile_out);
+        A_dst1 = A_dst{1};
+        A_dst2 = A_dst{2};
+
             
         par.redo = 0;
         par.verbose = 2;
@@ -90,8 +98,6 @@ end
 
 
 %% Create output directories architecture
-
-@ -113,11 +99,19 @@ double_model_dir = get_subdir_regex(main_dir, model_name{1});
 patient_list = {'PARKGAMEII_001_NB_a','PARKGAMEII_002_BM_a','PARKGAMEII_003_SM_c','PARKGAMEII_007_SD_a','PARKGAMEII_008_JR_a','PARKGAMEII_023_LJ_c','PARKGAMEII_025_CA_a','PARKGAMEII_028_PC_c','PARKGAMEII_033_DD','PARKGAMEII_039_KM_a','PARKGAMEII_043_PD_a','PARKGAMEII_044_CK_c','PARKGAMEII_047_BF_c','PARKGAMEII_048_SB_a','PARKGAMEII_052_HJ_c'}; 
 %patient_list = {'P_ARKGAMEII_009_HJ_c','P_ARKGAMEII_013_RP_c','P_ARKGAMEII_027_OR_a','P_ARKGAMEII_046_HJ_c','PARKGAMEII_053_LM_c'}; %exclu
 
@@ -107,24 +113,22 @@ for ipatient = 1: length(patient_list)
 end
 
 
-
 %% Job define model
 clear par
+
 par.sge = 0;
+par.run = 1;
+par.display = 0;
+
+par.mask_thr = 0.1;
+par.mask = 1; % if we want to use a personalised mask
+par.mask_path = {''};
+par.TR = 1.6;
 par.rp = 1;
-
-%% ts TAPAS
-
 par.file_reg = '^con_0001.nii';
 par.rp_regex = 'wts_multiple_regressors.txt';
 
-par.sge = 1;
-par.run = 0;
-par.display = 0;
 par.jobname = 'spm_glm_auto_dbl_def_RS';
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% TO CREATE a MODIFIED VERSION
-job_auto_RS_doublerun_model_specify(dirFonc, outdirs, par)
-%job_ending_rountines(job1,[],par);
 
-%return
+job_RS_doublerun_model_specify(dirFonc, outdirs, par)
+
--- job_RS_doublerun_model_specify.m
+++ job_RS_doublerun_model_specify.m
@@ -14,17 +14,17 @@ par = complet_struct(par, defpar);
 
 %%
 n = 1;
-for ip = 1 : length(dirFonc{1,:,:})
-    for ir = 1 : length(dirFonc{:,})
-        jobs{n}.spm.stats.fmri_spec.dir = dirOutput{n};
+for ip = 1 : size(dirFonc(:,1))
+    for ir = 1 : size(dirFonc(1,:))
+        jobs{n}.spm.stats.fmri_spec.dir = dirOutput{ip,ir};
         jobs{n}.spm.stats.fmri_spec.timing.units = 'secs';
         jobs{n}.spm.stats.fmri_spec.timing.RT = 1.6;
         jobs{n}.spm.stats.fmri_spec.timing.fmri_t = 16;
         jobs{n}.spm.stats.fmri_spec.timing.fmri_t0 = 8;
         %%
-        subjectRun1 = get_subdir_regex_files(dirFonc{i,1},par.file_reg);
+        subjectRun1 = get_subdir_regex_files(dirFonc{ip,ir}{1}, par.file_reg);
         unzip_volume(subjectRun1);
-        subjectRun1 = get_subdir_regex_files(dirFonc{i,1}, par.file_reg, struct('verbose',0));
+        subjectRun1 = get_subdir_regex_files(dirFonc{ip,ir}{1}, par.file_reg, struct('verbose',0));
 
         clear allVolumes
         allVolumes = spm_select('expand',cellstr(subjectRun1{1}));
@@ -39,14 +39,14 @@ for ip = 1 : length(dirFonc{1,:,:})
 
         % jobs{n}.spm.stats.fmri_spec.sess(1).multi_reg = {'/home/anna.skrzatek/data/nifti_test/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/S12_ACTIVATION/wts_multiple_regressors.txt'};
         if par.rp
-            jobs{n}.spm.stats.fmri_spec.sess(1).multi_reg = get_subdir_regex_files(dirFonc{i,1},par.rp_regex);
+            jobs{n}.spm.stats.fmri_spec.sess(1).multi_reg = get_subdir_regex_files(dirFonc{ip,ir}{1},par.rp_regex);
         end
         jobs{n}.spm.stats.fmri_spec.sess(1).hpf = 128;
         
         %%
-        subjectRun2 = get_subdir_regex_files(dirFonc{i,2},par.file_reg);
+        subjectRun2 = get_subdir_regex_files(dirFonc{ip,ir}{2},par.file_reg);
         unzip_volume(subjectRun2);
-        subjectRun2 = get_subdir_regex_files(dirFonc{i,2}, par.file_reg, struct('verbose',0));
+        subjectRun2 = get_subdir_regex_files(dirFonc{ip,ir}{2}, par.file_reg, struct('verbose',0));
         clear allVolumes2
         allVolumes2 = spm_select('expand',cellstr(subjectRun2{1}));
         
@@ -63,7 +63,7 @@ for ip = 1 : length(dirFonc{1,:,:})
 
         % jobs{n}.spm.stats.fmri_spec.sess(2).multi_reg = {'/home/anna.skrzatek/data/nifti_test/2020_07_27_PARKGAMEII_044_CK_27_07_2020_V2_c/S10_ACTIVATION/wts_multiple_regressors.txt'};
         if par.rp
-            jobs{n}.spm.stats.fmri_spec.sess(2).multi_reg = get_subdir_regex_files(dirFonc{i,2},par.rp_regex);
+            jobs{n}.spm.stats.fmri_spec.sess(2).multi_reg = get_subdir_regex_files(dirFonc{ip,ir}{2},par.rp_regex);
         end
 
         jobs{n}.spm.stats.fmri_spec.sess(2).hpf = 128;
@@ -75,7 +75,7 @@ for ip = 1 : length(dirFonc{1,:,:})
         
         if par.mask
             if par.mask_path{1} == ''
-                jobs{n}.spm.stats.fmri_spec.mask = unzip_volume(get_subdir_regex_files(dirFonc{i,2},'wbet.*mask'));
+                jobs{n}.spm.stats.fmri_spec.mask = unzip_volume(get_subdir_regex_files(dirFonc{ip,ir}{2},'wbet.*mask'));
             else
                 jobs{n}.spm.stats.fmri_spec.mask = unzip_volume(par.mask_path);
             end
