1ae2cc89efcf21b5306af1a3bb47dff3d0010088
askrzatek <skrzatek.a@gmail.com>
2022-01-21 17:03:09 +0100

    one test script with 2 main processes : the corel matrix per subject and vbm basic data load for future model specification (as in RS_secondlevel_ROI_par_group)

--- new_scripts_test.m
+++ new_scripts_test.m
@@ -117,51 +117,48 @@ end
 patients_dir_V1 = get_subdir_regex(Input, patient_list_V1); %%outdirs
 patients_dir = get_subdir_regex(Input, patient_list); %%outdirs
 
-%% SYMBOLIC LINK CREATION
-% manual within the terminal
-% vbm_outdirs = {fullfile(main_dir,'/VBM/PARKGAME_V1'),fullfile(main_dir,'/VBM/PARKGAME_a'),fullfile(main_dir,'/VBM/PARKGAME_c')}
-
-addpath /home/anna.skrzatek/MRI_analysis/
-for i = 1 : length(e)
-    dir_anat = e.getSerie('cat').toJob(0);
-        
-        fanat = e.gser('cat').getVolume('smwp1').toJob;
-    for ii = 1 : length(dir_anat)
-        clear par
-        %par.subdir        = 'tedana_vtd_mle';
-        par.subdir        = 'tedana009a1_vtd';
-        par.sge = 0;
-        par.run = 1;
-
-        par.regfile_regex = '^s6wts';
-
-        for i= 1:length(e)
-            wd = e(i).getSerie('RS').path;
-            reg_dir = char(get_subdir_regex(wd, par.subdir));
-            A_src = get_subdir_regex_files(reg_dir, par.regfile_regex);
-            regfile_out = char(A_src);
-            regfile_out = regfile_out(end-11:end);
-            A_dst = fullfile(wd, regfile_out);
-
-            par.redo = 0;
-            par.verbose = 2;
-            %par.run = 0;
-            par.run = 1;
-            par.jobname = sprintf('job_symbolic_link');
-            %par.jobname = sprintf('%s_%s_%s', 'job_symbolic_link', wd(end-46:end-16), par.subdir);
-            [job_session(i)] = r_movefile(A_src, A_dst, 'linkn', par);
-            job = [job_session];
-
-        end
-    end
-end
 
 %% ONCE THE INPUT STRUCTURE EXISTS
 % separate files in analysis groups : V1_all, PARK_c, PARK_a
 VBM_a = exam(Input,'PARK.*a$');
 VBM_c = exam(Input,'PARK.*[c,DD]$');
-VBM_all = exam(Input,'PARK.*a$') + exam(InputRS,'PARK.*[c,DD]$');
+VBM_all = exam(Input,'PARK.*a$') + exam(Input,'PARK.*[c,DD]$');
 V1_all = exam(Input,'PARK.*a$') + exam(Input,'PARK.*a_V1') + exam(Input,'PARK.*[c,DD]$') + exam(Input,'PARK.*c_V1');
+
+Sessions    = {'V1','V2'}%,'V2-V1'};
+VBM_a.addSerie('report','TIV',1);
+VBM_a.addSerie('mri','mri',1);
+
+VBM_c.addSerie('report','TIV',1);
+VBM_c.addSerie('mri','mri',1);
+
+VBM_all.addSerie('report','TIV',1);
+VBM_all.addSerie('mri','mri',1);
+
+for iS = 1 : length(Sessions)
+%PARK_a
+    
+    VBM_a.getSerie('mri').addVolume(sprintf('^smwp1_V%d',iS),sprintf('smwp1_V%d',iS));
+    VBM_a.getSerie('TIV').addVolume(sprintf('^cat_v_V%d',iS),sprintf('TIV_V%d',iS));    
+    
+%PARK_c
+    
+    VBM_c.getSerie('mri').addVolume(sprintf('^smwp1_V%d',iS),sprintf('smwp1_V%d',iS));
+    VBM_c.getSerie('TIV').addVolume(sprintf('^cat_v_V%d',iS),sprintf('TIV_V%d',iS));    
+    
+%all V1 & V2
+
+    VBM_all.getSerie('mri').addVolume(sprintf('^smwp1_V%d',iS),sprintf('smwp1_V%d',iS));
+    VBM_all.getSerie('TIV').addVolume(sprintf('^cat_v_V%d',iS),sprintf('TIV_V%d',iS));
+    
+end
+
+% FIRSTLEVEL for ALL PARKs add volumes for each ROI's contrast
+
+V1_all.addSerie('report','TIV',1);
+V1_all.gser('TIV').addVolume('cat_v_V1','TIV_V1',1)
+V1_all.addSerie('mri','mri',1);
+V1_all.gser('mri').addVolume('smwp1_V1','smwp1_V1',1)
 
 %% Regressors definition V1
 %% AGE
@@ -228,61 +225,101 @@ load TIV_V1.txt
 %% inspired from S.OUARAB
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% fetch Input dirs
-RSinput_dir = fullfile(main_dir,'/firstlevel_RS')
-cd (RSinput_dir)
+%% fetch Input & Output dirs
+main_dir = '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/';
+cd (main_dir)
+load e
 
-patient_regex = {'PARKGAMEII.*NB.*_a','PARKGAMEII.*BM.*_a','PARKGAMEII.*SM.*_c','PARKGAMEII.*SD.*_a','PARKGAMEII.*JR.*_a','PARKGAMEII.*LJ.*_c','PARKGAMEII.*CA.*_a','PARKGAMEII.*PC.*_c','PARKGAMEII.*DD.*','PARKGAMEII.*KM.*_a','PARKGAMEII.*PD.*_a','PARKGAMEII.*CK.*_c','PARKGAMEII.*BF.*_c','PARKGAMEII.*SB.*_a','PARKGAMEII_052.*HJ.*_c'}; %,'PARKGAMEII.*LM.*_c'};
+patient_regex_V1 = {'PARKGAMEII.*NB.*_a','PARKGAMEII.*BM.*_a','PARKGAMEII.*SM.*_c','PARKGAMEII.*SD.*_a','PARKGAMEII.*JR.*_a','PARKGAMEII.*LJ.*_c','PARKGAMEII.*CA.*_a','PARKGAMEII.*PC.*_c','PARKGAMEII.*DD.*','PARKGAMEII.*KM.*_a','PARKGAMEII.*PD.*_a','PARKGAMEII.*CK.*_c','PARKGAMEII.*BF.*_c','PARKGAMEII.*SB.*_a','PARKGAMEII_052.*HJ.*_c','PARKGAMEII_040.*RE.*_a$','PARKGAMEII_042.*RS.*_a$','PARKGAMEII_046.*HJ.*_c$','PARKGAMEII_053.*LM.*_a$'};
+patient_list_V1 = {'PARKGAMEII_001_NB_a','PARKGAMEII_002_BM_a','PARKGAMEII_003_SM_c','PARKGAMEII_007_SD_a','PARKGAMEII_008_JR_a','PARKGAMEII_023_LJ_c','PARKGAMEII_025_CA_a','PARKGAMEII_028_PC_c','PARKGAMEII_033_DD','PARKGAMEII_039_KM_a','PARKGAMEII_043_PD_a','PARKGAMEII_044_CK_c','PARKGAMEII_047_BF_c','PARKGAMEII_048_SB_a','PARKGAMEII_052_HJ_c','PARKGAMEII_040_RE_a_V1','PARKGAMEII_042_RS_a_V1','PARKGAMEII_046_HJ_c_V1','PARKGAMEII_053_LM_a_V1'};
+patient_regex = {'PARKGAMEII.*NB.*_a','PARKGAMEII.*BM.*_a','PARKGAMEII.*SM.*_c','PARKGAMEII.*SD.*_a','PARKGAMEII.*JR.*_a','PARKGAMEII.*LJ.*_c','PARKGAMEII.*CA.*_a','PARKGAMEII.*PC.*_c','PARKGAMEII.*DD.*','PARKGAMEII.*KM.*_a','PARKGAMEII.*PD.*_a','PARKGAMEII.*CK.*_c','PARKGAMEII.*BF.*_c','PARKGAMEII.*SB.*_a','PARKGAMEII_052.*HJ.*_c'};
 patient_list = {'PARKGAMEII_001_NB_a','PARKGAMEII_002_BM_a','PARKGAMEII_003_SM_c','PARKGAMEII_007_SD_a','PARKGAMEII_008_JR_a','PARKGAMEII_023_LJ_c','PARKGAMEII_025_CA_a','PARKGAMEII_028_PC_c','PARKGAMEII_033_DD','PARKGAMEII_039_KM_a','PARKGAMEII_043_PD_a','PARKGAMEII_044_CK_c','PARKGAMEII_047_BF_c','PARKGAMEII_048_SB_a','PARKGAMEII_052_HJ_c'};
 
-RSObj_a = exam(InputRS,'PARK.*a$');
-RSObj_c = exam(InputRS,'PARK.*[c,DD]$');
 
-ROIs = {'Caudate_L','Caudate_R','Cereb3_L','Cereb3_R','Cereb4_5_L','Cereb4_5_R','Cereb6_L','Cereb6_R','Cereb7b_L','Cereb7b_R','Cereb8_L','Cereb8_R','Cereb9_L','Cereb9_R','Cereb10_L','Cereb10_R','Cereb_Crus1_L','Cereb_Crus1_R','Cereb_Crus2_L','Cereb_Crus2_R','Cuneus_L','Cuneus_R','Insula_L','Insula_R','Pallidum_L','Pallidum_R','Parietal_Inf_L','Parietal_Inf_R','Parietal_Sup_L','Parietal_Sup_R','PCC','Postcentral_L','Postcentral_R','PPN_L','PPN_R','Precentral_L','Precentral_R','Precuneus_L','Precuneus_R','SMA_Ant_L','SMA_Ant_R','SMA_Post_L','SMA_Post_R','Vermis1_2','Vermis3','Vermis4_5','Vermis6','Vermis7','Vermis8','Vermis9','Vermis10'};
+input_dir = get_subdir_regex(fullfile(main_dir,'/firstlevel_RS'),patient_list);
+
+ROIs = {'Cereb3_L','Cereb3_R','Cereb4_5_L','Cereb4_5_R','Cereb6_L','Cereb6_R','Cereb7b_L','Cereb7b_R','Cereb8_L','Cereb8_R','Cereb9_L','Cereb9_R','Cereb10_L','Cereb10_R','Cereb_Crus1_L','Cereb_Crus1_R','Cereb_Crus2_L','Cereb_Crus2_R','Cuneus_L','Cuneus_R','Insula_L','Insula_R','Parietal_Inf_L','Parietal_Inf_R','Parietal_Sup_L','Parietal_Sup_R','Postcentral_L','Postcentral_R','Precentral_L','Precentral_R','Precuneus_L','Precuneus_R','SMA_Ant_L','SMA_Ant_R','SMA_Post_L','SMA_Post_R','Vermis1_2','Vermis3','Vermis4_5','Vermis6','Vermis7','Vermis8','Vermis9','Vermis10','Caudate_aal_L','Caudate_aal_R','Thalamus_aal_L','Thalamus_aal_R','Putamen_aal_L','Putamen_aal_R','Pallidum_aal_L','Pallidum_aal_R','PPN_YEB_L','PPN_YEB_R'};
 % ROIs = {'SMA_face_L','SMA_face_R','SMA_foot_L','SMA_foot_R','SMA_hand_L','SMA_hand_R'};
-
-dirFonc = cell(length(patient_regex),length(ROIs));
-RS_V1 = exam(main_dir, 'RR');
-RS_V2 = RS_V1;
-
-dir_RS   = e.getSerie('run_RS$').removeEmpty. toJob(0);
-%dir_RS   = cellstr(dir_RS(1:length(dir_RS)/2));
-dirFunc  = e.getSerie('tedana_RS').removeEmpty.toJob(0);
-
-dirStats = e.getSerie('run_RS$').mkdir('model','model_2'); % basic model ALFF %better denoising from tapas resliced masks
+dirROI  = '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/ROI_pariet_mot_premot_cereb_BG_PPN';
+fileROI = cellstr(char(gfile(dirROI,ROIs))); %fileROI = remove_regex(fileROI,'T1');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Verif VOI
-%% verification creation VOIs
-corelmatrix_dir = '/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti_test/RS_matrix';
+%% get VOIs for each run
+
 nRun   = 2;
-nSubj = length(patient_list);
+nSubj = length(input_dir);
 nROI  = length(ROIs)+1;
 
 voi_file = cell(nSubj,nROI,nRun);
+
 for subj = 1 : nSubj
-    patients_dir{subj} = get_subdir_regex(main_dir, patient_list{subj});
     for ir = 1 : nROI-1
        
        [~,roi_name] = fileparts(fileROI{ir});
        roi_name = roi_name(1:end-10);
        
-       mkdir(char(patients_dir{subj}),roi_name);
-       outdirs{subj,ir} =  get_subdir_regex(patients_dir{subj},roi_name);%% outdirs
-       
        for iRun = 1 : nRun
-           voi_file{subj,ir,iRun} = gfile(Stat_all{subj},sprintf('VOI_%s.*_%d.mat',roi_name,iRun));
+           %voi_file{subj,ir,iRun} = get_subdir_regex_files(input_dir{subj},sprintf('VOI_%s.*_%d.mat',roi_name,iRun));
+           voi_file(subj,ir,iRun) = get_subdir_regex_files(input_dir{subj},sprintf('VOI_%s.*_%d.mat',roi_name,iRun));
        end
     end
-    [~,roi_name] = fileparts(filePCC{subj});
-    roi_name = roi_name(5:end-2);
-    
-    mkdir(char(patients_dir{subj}),roi_name);
-    outdirs{subj,nROI} =  get_subdir_regex(patients_dir{subj},roi_name);%% outdirs
-       
     for iRun = 1 : nRun
-       voi_file{subj,nROI,iRun} = gfile(Stat_all{subj},sprintf('VOI_%s.*_%d.mat',roi_name,iRun));
+       %voi_file{subj,nROI,iRun} = get_subdir_regex_files(input_dir{subj},sprintf('VOI_PCC_%d.mat',iRun));
+       voi_file(subj,nROI,iRun) = get_subdir_regex_files(input_dir{subj},sprintf('VOI_PCC_%d.mat',iRun));
     end   
 end
+voi = voi_file;
+%fmat = get_subdir_regex_files(input_dir,ROIs);
+%fmat{1};
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 
+%%
+for subj = 1 : nSubj
+    for iRun = 1 : nRun
+        f1name = sprintf('/V%d_corr_matrix.csv',iRun);
+        f2name = sprintf('/V%d_pval_corr_matrix.csv',iRun);
+        fout1 = addprefixtofilenames(input_dir{subj},f1name);
+        fout2 = addprefixtofilenames(input_dir{subj},f2name);
+        
+        sujroi = voi(subj,:,iRun);
+        time_course_matrix = {};
+        parfor i = 1 : length(sujroi)
+            l = load(sujroi{i});
+            time_course_matrix{i} = l.Y;
+        end
+        time_course_matrix = cell2mat(time_course_matrix);
+        
+        [cor_mat, pval] = corr(time_course_matrix);
+
+%% text output        
+        
+        [~, roiname] = get_parent_path(sujroi);
+        roiname = nettoie_dir(change_file_extension(roiname,''))
+        
+        Tcor = array2table(cor_mat, 'VariableNames', roiname);
+        Tpval = array2table(pval, 'VariableNames', roiname);
+        
+        writetable(Tcor,fout1)
+        writetable(Tpval,fout2)
+        
+%% visual output        
+        t = sprintf('Correlation matrix V%d :',iRun);
+        roilabels = vertcat(ROIs(:),{'PCC'});
+        figure; imagesc(cor_mat)
+        title([t, patient_list{subj}], 'Interpreter', 'none')
+        ax = gca;
+        x = 0.5:54.5;
+        y = 0.5:54.5;
+        ax.XTick = x;
+        ax.YTick = y;
+        ax.XTickLabelRotation = 90;
+        ax.YTickLabelRotation = 0;
+        ax.XTickLabel = roilabels;
+        ax.YTickLabel = roilabels;
+        savefig(sprintf('%sV%d_cormat.fig',input_dir{subj},iRun))
+        close
+        
+    end
+    %% Could be a good place to get V1 and V2 table values and make a difference (individual deltas)
+    
+end
