0b351f5ac66dc4a59204a74e2e30f1c964b6467b
askrzatek <skrzatek.a@gmail.com>
2022-01-24 14:33:54 +0100

    Update pTFCE_adapt.m
    test pour ameliorer ptfce automatic processing

--- pTFCE_adapt.m
+++ pTFCE_adapt.m
@@ -3,6 +3,9 @@ function [pTFCE_Z, pTFCE_p] = pTFCE_adapt(SPMpath, con)
 load(SPMpath)
 rD = SPM.xVol.R(4);
 V = SPM.xVol.S;
+vol = spm_read_vols(V);
+
+[p nm e v] = spm_fileparts(V.fname);
 
 parts = strsplit(SPMpath, '/');
 SPMparent = sprintf('/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/%s/',parts{2:end-1});
