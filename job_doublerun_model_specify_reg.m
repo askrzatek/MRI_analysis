62cb221f499ae71f835b34eeb2d2caef5e1d7129
askrzatek <skrzatek.a@gmail.com>
2022-05-13 17:37:31 +0200

    getting the emg-specific glm model

--- job_doublerun_model_specify_reg.m
+++ job_doublerun_model_specify_reg.m
@@ -32,42 +32,35 @@ for i = 1 : length(dirFonc)
 
     %%
     %jobs{i}.spm.stats.fmri_spec.sess(1).cond(1) = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-   if ~exist emg_reg
+   if ~exist(emg_reg,'file')
       % jobs{i}.spm.stats.fmri_spec.sess(1).multi = {'/home/anna.skrzatek/data/behav/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/20200313T141827_PARKGAMEII_P044_CK_S01_13_03_2020_MRI_run01_SPM.mat'};
       jobs{i}.spm.stats.fmri_spec.sess(1).multi = onsets(i,1);
    else
-
-
-      %% QUESTION : comment organiser les emg_reg comme variable - 2 fichiers par session par sujet
-      %cond1 = load(emg_reg(i,1));
-
-      cond1 = load(emg_reg{i,1}(1))
-      jobs{i}.spm.stats.fmri_spec.sess(1).cond(1).onset = cond1.onset; % or maybe cond1(:,1) ?
-
-      %jobs{i}.spm.stats.fmri_spec.sess(1).cond(2) = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-      %cond2 = load(emg_reg(i,2));
-      cond2 = load(emg_reg{i,1}(2))
-      jobs{i}.spm.stats.fmri_spec.sess(1).cond(2).onset = cond2.onset; % or maybe cond2(:,1) ?
-    %% END OF QUESTION
-
-    
-      %% QUESTION : utiliser comme multi ou rajouter les conditions if positives pour creer les 2 conditions qui manquent ?
-      
+          
       stim = load(onsets(i,1));
 
-      if stim.name == ""
-         jobs{i}.spm.stats.fmri_spec.sess(1).cond(3).onset = stim.onset;
-      end
+      %stim.names == "Imaginary_Left"
+      jobs{i}.spm.stats.fmri_spec.sess(1).cond(1).onset = stim.onsets{4};
+      
+      %stim.names == "Imaginary_Right"
+      jobs{i}.spm.stats.fmri_spec.sess(1).cond(2).onset = stim.onsets{5};
+      
+    
+      %jobs{i}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {}); %% maybe add the age & sex regressors ?
 
-      if stim.name == ""
-         jobs{i}.spm.stats.fmri_spec.sess(1).cond(4).onset = stim.onset;
-      end
-    %% END OF QUESTION
+      ext_R = load(emg_reg{i,1,1}); %% not a good indexation method // will depend on the structure I create in firstlevel_tedana_double_run_reg
+      jobs{i}.spm.stats.fmri_spec.sess(1).regress(1) = ext_R;
+      ext_L = load(emg_reg{i,1,2}); %% not a good indexation method
+      jobs{i}.spm.stats.fmri_spec.sess(1).regress(2) = ext_L;
+
+      fle_R = load(emg_reg{i,1,3});
+      jobs{i}.spm.stats.fmri_spec.sess(1).regress(3) = fle_R;
+      fle_L = load(emg_reg{i,1,4});
+      jobs{i}.spm.stats.fmri_spec.sess(1).regress(4) = fle_L;
 
     end
-
-    jobs{i}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {}); %% maybe add the age & sex regressors ?
-
+    
+    
     % jobs{i}.spm.stats.fmri_spec.sess(1).multi_reg = {'/home/anna.skrzatek/data/nifti_test/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/S12_ACTIVATION/wts_multiple_regressors.txt'};
     if par.rp
         jobs{i}.spm.stats.fmri_spec.sess(1).multi_reg = get_subdir_regex_files(dirFonc{i,1},par.rp_regex);
@@ -90,35 +83,31 @@ for i = 1 : length(dirFonc)
     %%
     %jobs{i}.spm.stats.fmri_spec.sess(1).cond(1) = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
 
-    if ~exist emg_reg
-         % jobs{i}.spm.stats.fmri_spec.sess(2).multi = {'/home/anna.skrzatek/data/behav/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/20200313T141827_PARKGAMEII_P044_CK_S01_13_03_2020_MRI_run01_SPM.mat'};
-         jobs{i}.spm.stats.fmri_spec.sess(2).multi = onsets(i,2);
-    else
+    if ~exist(emg_reg,'file')
+      % jobs{i}.spm.stats.fmri_spec.sess(1).multi = {'/home/anna.skrzatek/data/behav/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/20200313T141827_PARKGAMEII_P044_CK_S01_13_03_2020_MRI_run01_SPM.mat'};
+      jobs{i}.spm.stats.fmri_spec.sess(2).multi = onsets(i,2);
+   else
+          
+      stim = load(onsets(i,2));
 
-       %% QUESTION : comment organiser les emg_reg comme variable - 2 fichiers par session par sujet
-       cond1 = load(emg_reg{i,2}(1));
-       jobs{i}.spm.stats.fmri_spec.sess(2).cond(1).onset = cond1.onset;
+      %stim.names == "Imaginary_Left"
+      jobs{i}.spm.stats.fmri_spec.sess(2).cond(1).onset = stim.onsets{4};
+      
+      %stim.names == "Imaginary_Right"
+      jobs{i}.spm.stats.fmri_spec.sess(2).cond(2).onset = stim.onsets{5};
+      
+    
+      %jobs{i}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {}); %% maybe add the age & sex regressors ?
 
-       %jobs{i}.spm.stats.fmri_spec.sess(1).cond(2) = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {}, 'orth', {});
-       cond2 = load(emg_reg{i,2}(2));
-       jobs{i}.spm.stats.fmri_spec.sess(2).cond(2).onset = cond2.onset;
-       %% END OF QUESTION
+      ext_R = load(emg_reg{i,2,1});
+      jobs{i}.spm.stats.fmri_spec.sess(2).regress(1) = ext_R;
+      ext_L = load(emg_reg{i,2,2});
+      jobs{i}.spm.stats.fmri_spec.sess(2).regress(2) = ext_L;
 
-       
-       %% QUESTION : utiliser comme multi ou rajouter les conditions if positives pour creer les 2 conditions qui manquent ?
-       % jobs{i}.spm.stats.fmri_spec.sess(1).multi = {'/home/anna.skrzatek/data/behav/2020_03_13_PARKGAMEII_044_CK_13_03_2020_V1_c/20200313T141827_PARKGAMEII_P044_CK_S01_13_03_2020_MRI_run01_SPM.mat'};
-       stim = load(onsets(i,2));
-
-       if stim.name == ""
-         jobs{i}.spm.stats.fmri_spec.sess(2).cond(3).onset = stim.onset;
-       else skip = [skip stim.name]
-       end
-
-       if stim.name == ""
-         jobs{i}.spm.stats.fmri_spec.sess(2).cond(4).onset = stim.onset;
-       else skip = [skip stim.name]
-       end
-       %% END OF QUESTION
+      fle_R = load(emg_reg{i,2,3});
+      jobs{i}.spm.stats.fmri_spec.sess(2).regress(3) = fle_R;
+      fle_L = load(emg_reg{i,2,4});
+      jobs{i}.spm.stats.fmri_spec.sess(2).regress(4) = fle_L;
 
     end
 
